ebd5a469261cef692f87ed26d1470c8f4b40d679
suriya159 <132151294+suriya159@users.noreply.github.com>
2023-07-12 11:11:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var on_btn =document.getElementById("on");
+var off_btn =document.getElementById("off");
+var img =document.getElementById("bulb");
+
+function bulp_on(){
+    img.src="./images/download.jpg";
+}
+function bulp_off(){
+    img.src="./images/download2.jpg";
+}
